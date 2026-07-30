3437ca16c6199a4027293ab7a18e27f144495c65
Alauddin Ansari <alauddin4edu@gmail.com>
2026-07-30 22:28:49 +0530

    m2.f4.express_app_and_health_route

---
+++ server/src/app.ts
@@ -0,0 +1,28 @@
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+import { config } from "./config.js";
+import { healthRouter } from "./routes/health.js";
+
+export const app = express();
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: config.clientOrigin,
+  })
+);
+
+app.use(express.json());
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+  })
+);
+
+app.use(healthRouter);
