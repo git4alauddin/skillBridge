71274f2a66c29d711d920cb15e5584906f74199b
Alauddin Ansari <alauddin4edu@gmail.com>
2026-07-30 21:36:40 +0530

    m2.f3.backend_folder_and_config

---
+++ server/src/config.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const config = {
+  nodeEnv: process.env.NODE_ENV ?? "development",
+  port: Number(process.env.PORT ?? 4000),
+  clientOrigin: process.env.CLIENT_ORIGIN ?? "http://localhost:5173",
+};
